6d40a7dfc5549312737ea42e318de3a0e4c969de
Sonarpratik <sonarpratik02.ps@gmail.com>
2025-01-22 18:06:48 +0530

    copied

--- server.js
+++ server.js
@@ -35,6 +35,7 @@ app.use(
 app.options("*", cors());
 app.use(bodyParser.json({ limit: '100mb' }));
 app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
 
 // app.use(bodyParser.json());
 app.use("/api", userRoutes);
